e54a0ccba334c116564dc05f0c993765df1169d5
Tom Benbow <tom@Toms-iMac.local>
2015-12-07 18:42:55 -0600

    removed scroll behaviors - refactored css

--- js/global.js
+++ js/global.js
@@ -1,26 +1,19 @@
-/*
-
-TODO:
-- Make code more modular (pubsub)
-*/
-
-
 $(document).ready(function() {
 
 	// Bindings
-	$('ul.pagedots').mouseenter( pagedotsOver ).mouseleave( pagedotsOut );
-	$('[data-action="scrollJump"]').mouseenter( dotJump );
-	$('[data-action="arrowJump"]').click( arrowJump );
+	//$('ul.pagedots').mouseenter( pagedotsOver ).mouseleave( pagedotsOut );
+	//$('[data-action="scrollJump"]').mouseenter( dotJump );
+	//$('[data-action="arrowJump"]').click( arrowJump );
 	$('body').on( 'click','[data-action="modalOpen"]', modalOpen );
 	$('body').on( 'click','[data-action="modalClose"]', modalClose);
 
-	init();
+	//init();
 });
 
 
-function init() {
-	render();
-}
+// function init() {
+// 	render();
+// }
 
 
 // Scroll Stop Detection
@@ -59,93 +52,47 @@ function scrollStop() {
 		currentID = $('.pagesections > section:eq(' + index + ')').attr('id');
 
 	// Animate to nearest section
-	$('html, body').animate({
-	 	scrollTop: scrollTo
-	}, 300, 'swing', function() {
-	 	updateHash(currentID);
-	});
+	// $('html, body').animate({
+	//  	scrollTop: scrollTo
+	// }, 300, 'swing', function() {
+	//  	updateHash(currentID);
+	// });
+
+	updateHash(currentID);
 }
-
-
-// Update Dots On Scroll
-//----------------------------------------------------
-$(window).scroll($.debounce( 200, true, function(){
-
-	// Leave if modal or menu is active
-	if ( $('.modal.is-active').length || $('body.is-menu-active').length ) {
-		return false;
-	}
-	
-	var currentScroll = $(window).scrollTop(),
-		windowHeight = $(window).height(),
-		currentPercent = (currentScroll % windowHeight) / windowHeight,
-		index = Math.round(currentScroll / windowHeight);
-
-	// Update Dots
-	$('.pagedots .is-active').removeClass('is-active');
-	$('.pagedots li:eq(' + index + ') a').addClass('is-active');
-}));
-
 
 // Update Layout On Resize
 //----------------------------------------------------
-$(window).resize($.debounce( 20, true, function(){
-	var currentSection = getCurrentSection(),
-		currentId = currentSection.id;
+// $(window).resize($.debounce( 20, true, function(){
+// 	var currentSection = getCurrentSection(),
+// 		currentId = currentSection.id;
 
-	jumpToSection(currentId);
-}));
+// 	jumpToSection(currentId);
+// }));
 
 
 // SIMPLE SPA FRAMEWORK
 //----------------------------------------------------
 $(window).on('hashchange', function(){
-    render();
+	console.log('hash change');
+    //render();
 });
 
 // Render page
 function render(hash) {
-	var currentSection = getCurrentSection(),
-		currentIndex = currentSection.index,
-		totalSections = $('.pagesections > section').length,
-		pagesections = $('.pagesections');
-
-	// First Page
-	if (currentIndex == 0) {
-		$('.logo').addClass('is-hidden');
-		$('.pagearrows li:first-child').addClass('is-hidden');
-		$('.pagearrows li:last-child').removeClass('is-hidden');
-	}
-
-	// Last Page
-	else if (currentIndex == totalSections-1) {
-		$('.logo').removeClass('is-hidden');
-		$('.pagearrows li:first-child').removeClass('is-hidden');
-		$('.pagearrows li:last-child').addClass('is-hidden');
-	}
-
-	// Other Pages
-	else {
-		$('.logo').removeClass('is-hidden');
-		$('.pagearrows li').removeClass('is-hidden');
-	}
-
-	// All Pages
-	$('.pagedots .is-active').removeClass('is-active');
-	$('.pagedots li:eq(' + currentIndex + ') a ').addClass('is-active');
+	console.log('rendering ' + hash)
 }
 
 // Update hash
 function updateHash(hash) {
-	document.location.hash = hash;
+	//document.location.hash = hash;
 }
 
 
-// UTILITES
+// SECTION NAVIGATION UTILITES
 //----------------------------------------------------
 function getCurrentSection() {
 	var currentSection = {};
-
 	currentSection.id = document.location.hash;
 	currentSection.element = $(currentSection.id);
 	currentSection.index = $('.pagesections > section').index(currentSection.element);
@@ -154,29 +101,31 @@ function getCurrentSection() {
 
 function getTotalSections() {
 	var totalSections = $('.pagesections > section').length;
-
 	return totalSections;
 }
 
 function lockSections() {
+	console.log('lock sections');
+
 	var windowHeight = $(window).height(),
 		currentSection = getCurrentSection(),
 		scrollTarget = (windowHeight * currentSection.index) * -1;
-
-		$('.pagesections').css({ 'position' : 'fixed', 'top' : scrollTarget });
+	$('.pagesections').css({ 'position' : 'fixed', 'top' : scrollTarget });
 }
 
 function unlockSections() {
+	console.log('unlock sections');
+
 	var windowHeight = $(window).height(),
 		currentSection = getCurrentSection(),
 		scrollTarget = (windowHeight * currentSection.index) * -1;
-
 	$('.pagesections').css({ 'position' : 'relative', 'top' : 'auto' });
-	console.log(currentSection.id);
 	jumpToSection(currentSection.id);
 }
 
 function scrollToSection(id) {
+	console.log('scroll to section');
+
 	var target = $(id),
 		index = $('.pagesections > section').index(target),
 		scrollTarget = $(window).height() * index;
@@ -189,62 +138,14 @@ function scrollToSection(id) {
 }
 
 function jumpToSection(id) {
+	console.log('jump to section');
+
 	var target = $(id),
 		index = $('.pagesections > section').index(target),
 		scrollTarget = $(window).height() * index;
 
 	// Instantly jump to Scroll Target
 	$('html, body').scrollTop( scrollTarget );
-}
-
-
-// PAGEDOT NAVIGATION
-//----------------------------------------------------
-function pagedotsOver(){
-	$('body').addClass('is-menu-active');
-}
-
-function pagedotsOut(){
-	var hash = $(this).find('a.is-active').attr('href');
-
-	$('body').removeClass('is-menu-active');
-	updateHash(hash);
-	jumpToSection(hash);
-}
-
-function dotJump(e) {
-	var hash = $(this).attr('href');
-
-	$('.pagedots .is-active').removeClass('is-active');
-	$(this).addClass('is-active');
-	scrollToSection(hash);
-}
-
-
-// NAVIGATION ARROWS
-//----------------------------------------------------
-function arrowJump(e){
-	var direction = $(this).attr('href'),
-		currentSection = getCurrentSection(), // The current section index
-		index = currentSection.index,
-		scrollTo,
-		hash;
-
-	e.preventDefault();
-	if (direction == '#next') {
-		index += 1;
-	} else if (direction == '#prev') {
-		index -= 1;
-	}
-	scrollTo = $('.pagesections > section:eq(' + index + ')').offset().top + 'px';
-	newHash = $('.pagesections > section:eq(' + index + ')').attr('id');
-
-	// Animate Section
-	$('html, body').animate({
-		scrollTop: scrollTo
-	}, 500, 'swing', function() {
-		updateHash(newHash);
-	});
 }
 
 
@@ -265,7 +166,6 @@ function modalOpen(e){
 	// Inject Modal
 	modalInject(url);
 }
-
 
 function modalInject(url) {
 	var windowHeight = $(window).height();
